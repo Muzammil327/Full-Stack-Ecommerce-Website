6d420014c3ec85c8a52a5532e71c89f10b813b2d
Muzammil327 <mmuzammiloff327@gmail.com>
2024-05-01 09:37:30 +0500

    updated

--- src/app/api/products/singleProduct/route.ts
+++ src/app/api/products/singleProduct/route.ts
@@ -1,29 +1,29 @@
-import Product from "@/src/models/productModel";
-import connectDB from "@/src/utils/db";
-import { NextRequest, NextResponse } from "next/server";
+// import Product from "@/src/models/productModel";
+// import connectDB from "@/src/utils/db";
+// import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(
-  req: NextRequest,
-  { params }: { params: { id: string } }
-) {
+// export async function GET(
+//   req: NextRequest,
+//   { params }: { params: { id: string } }
+// ) {
 
-  const productId = params.id;
+//   const productId = params.id;
 
-  try {
-    await connectDB();
-    const product = await Product.findById(productId);
-    if (!product) {
-      return NextResponse.json({ error: "Product not found" });
-    }
+//   try {
+//     await connectDB();
+//     const product = await Product.findById(productId);
+//     if (!product) {
+//       return NextResponse.json({ error: "Product not found" });
+//     }
 
-    return NextResponse.json({
-      success: true,
-      product,
-    });
-  } catch (error) {
-    return NextResponse.json({
-      success: false,
-      error: "Error fetching product",
-    });
-  }
-}
+//     return NextResponse.json({
+//       success: true,
+//       product,
+//     });
+//   } catch (error) {
+//     return NextResponse.json({
+//       success: false,
+//       error: "Error fetching product",
+//     });
+//   }
+// }
