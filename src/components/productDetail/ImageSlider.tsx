1870a233716b2db8c786838a59881098629d14a6
Muzammil327 <mmuzammiloff327@gmail.com>
2024-06-14 18:09:34 +0500

    updated 6/14/2024

--- src/components/productDetail/ImageSlider.tsx
+++ src/components/productDetail/ImageSlider.tsx
@@ -18,57 +18,30 @@ import { EffectFlip, Pagination, Navigation } from "swiper/modules";
 import Image from "next/image";
 
 export default function ProductDetailImageSlider({ response }: any) {
-  const [swiperReady, setSwiperReady] = useState(false);
-
-  const hasSliderImages =
-    Array.isArray(response.slider) && response.slider.length > 0;
-
   return (
     <div className="img">
-      {!swiperReady && (
-        <div className="loading-spinner">
-          <Image
-            src={`${response.image}`}
-            width={1080}
-            height={1080}
-            alt=""
-            onLoad={() => setSwiperReady(true)}
-          />
-        </div>
-      )}
 
-      {swiperReady && hasSliderImages ? (
-        <Swiper
-          effect={"flip"}
-          grabCursor={true}
-          autoplay={{ delay: 4500, disableOnInteraction: false }}
-          loop={true}
-          modules={[EffectFlip, Pagination, Navigation, Autoplay]}
-          className="mySwipe"
-        >
-          {response.slider.map((index: string, slideIndex: number) => (
-            <SwiperSlide key={slideIndex}>
+      <Swiper
+        effect={"flip"}
+        grabCursor={true}
+        autoplay={{ delay: 4500, disableOnInteraction: false }}
+        loop={true}
+        modules={[EffectFlip, Pagination, Navigation, Autoplay]}
+        className="mySwipe"
+      >
+        {response.slider.map((index: string, slideIndex: number) => (
+          <SwiperSlide key={slideIndex}>
+            <div>
               <Image
                 src={`https://backend-full-stack-ecommerce-website.vercel.app/uploadSliderImage/${index}`}
                 width={1080}
                 height={1080}
                 alt=""
               />
-            </SwiperSlide>
-          ))}
-        </Swiper>
-      ) : (
-        swiperReady && (
-          <div className="fallback-image">
-            <Image
-              src={`${response.image}`}
-              width={1080}
-              height={1080}
-              alt="Fallback Image"
-            />
-          </div>
-        )
-      )}
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </div>
   );
 }
