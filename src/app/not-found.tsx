b19da89cbc5eec99ca688dd5f57ee71a398ec544
Muzammil327 <mmuzammiloff327@gmail.com>
2024-06-29 08:40:10 +0500

    updated 6/29/2024 8:40

---
+++ src/app/not-found.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function NotFound() {
+  return (
+    <>
+      <div className="hero bg-slate-200 py-40 flex items-center justify-center">
+        <h1 className="text-4xl font-bold capitalize">Not Found</h1>
+      </div>
+    </>
+  );
+}
