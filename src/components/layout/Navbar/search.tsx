c05c688f64d9afda5b5ee2b57de6b3e95667978e
Muzammil327 <mmuzammiloff327@gmail.com>
2024-07-08 17:50:57 +0500

    updated: 7/8/2024 5:50

--- src/components/layout/Navbar/search.tsx
+++ src/components/layout/Navbar/search.tsx
@@ -9,7 +9,7 @@ import {
   DropdownMenuTrigger,
 } from "@/src/components/ui/dropdown-menu";
 import { FaSearch } from "react-icons/fa";
-import { Input } from "../../ui/input";
+import { ShadcnUIInput } from "@/src/components/shadcnUI/Shadcnui-Input";
 
 export function SearchIcon() {
   return (
@@ -20,7 +20,7 @@ export function SearchIcon() {
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent className="md:w-[600px] w-full mt-4 py-4 px-4 flex items-center gap-2">
-        <Input placeholder="Search for Products" type="text" />
+        <ShadcnUIInput placeholder="Search for Products" type="text" />
         <Button variant="paddingWidth" round="md">
         <FaSearch />
         </Button>
---
+++ src/components/shadcnUI/Shadcnui-Input.tsx
@@ -0,0 +1,25 @@
+import * as React from "react";
+
+import { cn } from "@/lib/utils";
+
+export interface InputProps
+  extends React.InputHTMLAttributes<HTMLInputElement> {}
+
+const ShadcnUIInput = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ className, type, ...props }, ref) => {
+    return (
+      <input
+        type={type}
+        className={cn(
+          "flex h-12 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus:outline-none focus:ring-indigo-500 focus:border-indigo-500",
+          className
+        )}
+        ref={ref}
+        {...props}
+      />
+    );
+  }
+);
+ShadcnUIInput.displayName = "ShadcnUIInput";
+
+export { ShadcnUIInput };
